113aa762857a4a9bddce97a9901fe58c7d38e78c
unknown <hindrowahid@gmail.com>
2025-03-06 17:46:43 +0700

    add migration, add jetstream

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('phone')->nullable();
             $table->string('address')->nullable();
-            $table->string('usertype')->default(0);
+            $table->string('user_type')->default(0);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
