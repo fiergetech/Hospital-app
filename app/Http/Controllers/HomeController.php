cdb6c582bf31f9ef3667ba8bc81397fe839bfd29
“fiergetech” <hindrowahid@gmail.com>
2025-03-19 13:29:43 +0700

    create appointment function, still bug on user login

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use App\Models\doctor;
+use App\Models\appointment;
 
 class HomeController extends Controller
 {
@@ -43,6 +44,27 @@ class HomeController extends Controller
         {
         $doctor = doctor::all();
 
-        return view('user.home', compact('doctor'));        }
+        return view('user.home', compact('doctor'));        
+    }
+    }
+
+    public function appointment(Request $request)
+    {
+        $data = new appointment;
+        $data->name = $request->name;
+        $data->email = $request->email;
+        $data->date = $request->date;
+        $data->phone = $request->number;
+        $data->doctor = $request->doctor;
+        $data->date = $request->date;
+        $data->message = $request->message;
+        $data->status = 'In Progress';
+
+        if (Auth::id())
+        {
+            $data->user_id = Auth::user()->id;
+        }
+        $data->save();
+        return redirect()->back()->with('message', 'Reservasi berhasil dikirim');
     }
 }
